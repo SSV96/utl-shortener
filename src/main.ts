78a9c004cd234b7bb3fe164c4070c33cf4fb1970
SSV96 <sathyaswaroopvandavasi@gmail.com>
2025-02-12 15:16:29 +0530

    updated work flow

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,16 @@ async function bootstrap() {
   app.use(passport.initialize());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Url Shortener API')
+    .setDescription('Carry Your URL Handy')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('url-shortener-app')
     .build();
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+  app.setGlobalPrefix('api');
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
